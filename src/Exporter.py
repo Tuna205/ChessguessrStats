0699fde33b625efc2f27cc1231602cb32e413c12
tuna545 <tuna205@gmail.com>
2024-03-17 12:23:36 +0100

    export now saves dates

--- src/Exporter.py
+++ src/Exporter.py
@@ -7,5 +7,5 @@ class Exporter:
     def export_to_excel(df_dict, file_name):
         writer = pd.ExcelWriter(file_name, engine='xlsxwriter')
         for sheet_name, df in df_dict.items():
-            df.to_excel(writer, sheet_name=sheet_name, index=False)
+            df.to_excel(writer, sheet_name=sheet_name)
         writer.close()
